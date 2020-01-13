65fa392d7ec0264b48a6ec053a3efec1283e4d58
Franco Canedo <francanedo1@hotmail.com>
2020-01-13 20:49:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,12 @@
 def roll_call_dwarves(dwarves)
-  array = []
+  
   dwarves.each_with_index { |item, index|
     puts "#{index+1}. #{item}"
   }
 end
 
 def summon_captain_planet(planets)
+  array = []
   
 end
 
